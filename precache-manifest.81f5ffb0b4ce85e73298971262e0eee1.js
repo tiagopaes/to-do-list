34cabf6352d9d4e70bda6881a1f7eab32ec10767
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-06-10 16:58:26 +0000

    Deploy tiagopaes/to-do-list to github.com/tiagopaes/to-do-list.git:gh-pages

---
+++ precache-manifest.81f5ffb0b4ce85e73298971262e0eee1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7f37f3e0f048ba4f9bcd784eb9e82b0",
+    "url": "/to-do-list/index.html"
+  },
+  {
+    "revision": "f534eb281abcc61a9b75",
+    "url": "/to-do-list/js/app.97df2e0e.js"
+  },
+  {
+    "revision": "c8dcc6e7ec73c271d630",
+    "url": "/to-do-list/js/chunk-vendors.72dccd27.js"
+  },
+  {
+    "revision": "fca1f606eb8f210c577135e0479ee6e8",
+    "url": "/to-do-list/manifest.json"
+  },
+  {
+    "revision": "219ceffb5ebbf5a18a7c4025e7fef71b",
+    "url": "/to-do-list/privacy_policy.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/to-do-list/robots.txt"
+  }
+]);
